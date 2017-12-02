130ffdd0a8a28df1de688c5c75aa01aea0072395
Spencer <spencerflem@gmail.com>
2017-12-02 15:17:51 -0500

    initial commit

---
+++ desktop/src/com/spensoft/testgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spensoft.testgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spensoft.testgame.TestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestGame(), config);
+	}
+}
---
+++ html/src/com/spensoft/testgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.spensoft.testgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.spensoft.testgame.TestGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TestGame();
+        }
+}
